4c7bdf15182cf54344744d7066f4762a1659f3a5
Tanner Smith <tanner.smith1125@gmail.com>
2023-03-17 15:49:34 -0700

    Max profit

---
+++ src/topics/sliding_window/MaxProfit.java
@@ -0,0 +1,20 @@
+package topics.sliding_window;
+
+public class MaxProfit {
+
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int left = 0, right = 1;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                left = right;
+            } else {
+                max = Math.max(max, prices[right] - prices[left]);
+            }
+            right++;
+        }
+
+        return max;
+    }
+}
