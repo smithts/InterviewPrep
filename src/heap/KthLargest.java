ada78c979ee4fa9b888fa7c9858760589dc804e8
Tanner Smith <tanner.smith1125@gmail.com>
2023-04-06 10:50:02 -0700

    Additional Exercises

---
+++ src/heap/KthLargest.java
@@ -0,0 +1,35 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+        while (pq.size() > k) {
+            pq.poll();
+        }
+    }
+
+    public int add(int n) {
+        if (pq.size() < k)
+            pq.offer(n);
+        else if (pq.peek() < n) {
+            pq.poll();
+            pq.offer(n);
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
